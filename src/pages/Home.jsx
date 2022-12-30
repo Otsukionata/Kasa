ae7779141cb50667cdbcf69fae77adf461d81f61
Otsukionata <e-ramassamy@laposte.net>
2022-12-30 10:49:55 +0100

    composant Card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "../assets/victor-martin-2FM7RP20SHQ-unsplash.jpg";
+import Card from "../components/Card";
+import Flats from "../data/logements.json";
 
 export default function Home() {
   return (
@@ -9,6 +11,9 @@ export default function Home() {
         <img src={Image} alt="" className="cover__img" />
       </div>
       <div className="gallery"></div>
+      {Flats.map(({ id, title, cover }) => (
+        <Card key={id} title={title} id={id} cover={cover} />
+      ))}
     </main>
   );
 }
