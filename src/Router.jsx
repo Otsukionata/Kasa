16aa4f1cb8964f1b8fb3b591f9530ad85d286f84
Otsukionata <e-ramassamy@laposte.net>
2023-01-03 14:18:18 +0100

    Correction Routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 
 import Home from "./pages/Home";
+import About from "./pages/About";
 import FlatDetail from "./pages/FlatDetail";
 import ErrorPage from "./pages/ErrorPage";
 
