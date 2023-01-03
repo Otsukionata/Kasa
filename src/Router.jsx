6c49f678e992df2b7ac33cfbe160c448df70f0ab
Otsukionata <e-ramassamy@laposte.net>
2023-01-03 14:16:58 +0100

    Mise à jour Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,23 @@
+import React, { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import React from "react";
+import Header from "./components/Header";
+
 import Home from "./pages/Home";
+import FlatDetail from "./pages/FlatDetail";
+import ErrorPage from "./pages/ErrorPage";
 
 export default function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-    </Routes>
+    <React.Fragment>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="housing/:id" element={<FlatDetail />} />
+
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </React.Fragment>
   );
 }
