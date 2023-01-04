d6c476bd8cc3d2cfe100382a9ffb7b0c419b203e
Otsukionata <e-ramassamy@laposte.net>
2023-01-04 13:45:28 +0100

    Page logement détail

--- src/pages/FlatDetail.jsx
+++ src/pages/FlatDetail.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Slideshow from "../components/Slideshow";
 
-export default function FlatDetail() {
+export default function FlatDetail({ id }) {
+  const { data } = useParams;
+
+  useEffect(() => {
+    fetch("../datas/logements.json").then((response) =>
+      response
+        .json()
+        .then((flatData) => console.log(flatData))
+        .catch((error) => console.error(error))
+    );
+  }, []);
+
   return (
     <main>
       <Slideshow />
+      <div className="flat">
+        <div className="flat__details">
+          <h1>Magnifique appartement proche Canal Saint Martin</h1>
+          <p className="test">Référence du logement : {data}</p>
+          <h2>Île-de-France, Paris 10e</h2>
+        </div>
+      </div>
     </main>
   );
 }
