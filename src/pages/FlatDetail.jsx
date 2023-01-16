8c08c2f48d7507562c0a873d5a1d310c09e9f55d
Otsukionata <e-ramassamy@laposte.net>
2023-01-16 15:25:15 +0100

    correction page FlatDetails

--- src/pages/FlatDetail.jsx
+++ src/pages/FlatDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useParams } from "react-router-dom";
 
 import appartmentList from "../data/logements.json";
@@ -19,53 +19,53 @@ export default function Flat() {
   }
 
   return (
-      <main>
-        <Slideshow pictures={getFlat.pictures} />
-        <div className="flat">
-          <div className="flat__details">
-            <h1>{getFlat.title}</h1>
-            <h2>{getFlat.location}</h2>
-            <div className="flat__tags">
-              {getFlat.tags.map((tag) => (
-                <div
-                  className="flat__tagTxt"
-                  key={`${getFlat.tags}-${Math.random()}`}
-                >
-                  {tag}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="flat__profile">
-            <div className="flat__profile--infos">
-              <p>{getFlat.host.name}</p>
-              <div className="flat__profile--infos picture">
-                <img
-                  src={getFlat.host.picture}
-                  alt="Photo de profil"
-                  className="carousel__img"
-                />
+    <main>
+      <Slideshow pictures={getFlat.pictures} />
+      <div className="flat">
+        <div className="flat__details">
+          <h1>{getFlat.title}</h1>
+          <h2>{getFlat.location}</h2>
+          <div className="flat__tags">
+            {getFlat.tags.map((tag) => (
+              <div
+                className="flat__tagTxt"
+                key={`${getFlat.tags}-${Math.random()}`}
+              >
+                {tag}
               </div>
-            </div>
-            <div className="flat__profile--ratings">
-              <Ratings rating={getFlat.rating} />
-            </div>
+            ))}
           </div>
         </div>
-        <div className="accommodations">
-          <Collapse
-            className={"accommodations__txt"}
-            title={"Description"}
-            description={getFlat.description}
-          />
-          <Collapse
-            className={"accommodations__txt"}
-            title={"Équipements"}
-            description={getFlat.equipments.map((item) => (
-              <li key={Math.random()}>{item}</li>
-            ))}
-          />
+        <div className="flat__profile">
+          <div className="flat__profile--infos">
+            <p>{getFlat.host.name}</p>
+            <div className="flat__profile--infos picture">
+              <img
+                src={getFlat.host.picture}
+                alt="Photo de profil"
+                className="carousel__img"
+              />
+            </div>
+          </div>
+          <div className="flat__profile--ratings">
+            <Ratings rating={getFlat.rating} />
+          </div>
         </div>
-      </main>
+      </div>
+      <div className="accommodations">
+        <Collapse
+          className={"accommodations__txt"}
+          title={"Description"}
+          description={getFlat.description}
+        />
+        <Collapse
+          className={"accommodations__txt"}
+          title={"Équipements"}
+          description={getFlat.equipments.map((item) => (
+            <li key={Math.random()}>{item}</li>
+          ))}
+        />
+      </div>
+    </main>
   );
 }
