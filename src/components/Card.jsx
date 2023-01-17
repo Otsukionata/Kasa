14900ad823d128299f52b004fe4e7035b8330eca
Otsukionata <e-ramassamy@laposte.net>
2023-01-17 10:29:45 +0100

    correction erreur de nommage CSS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Card({ id, cover, title }) {
   return (
-    <Link id={id} className="gallery_thumbnail" to={`housing/${id}`}>
-      <img src={cover} alt="" className="gallery__img" />
-      <div className="gallery__title">
-        <p className="gallery__title-txt">{title}</p>
+    <Link id={id} className="cards__thumbnail" to={`housing/${id}`}>
+      <img src={cover} alt="" className="cards__img" />
+      <div className="cards__title">
+        <p className="cards__title-txt">{title}</p>
       </div>
     </Link>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import "./styles/style.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
