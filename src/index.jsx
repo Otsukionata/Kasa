fa1997ae8ccc0b69d7646e2a7dbcb99ee14e94ed
Otsukionata <e-ramassamy@laposte.net>
2023-01-24 17:24:48 +0100

    correction erreur strictmode

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -7,7 +7,9 @@ import "./styles/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
